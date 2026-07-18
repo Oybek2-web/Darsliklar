2a95366a3f8170a9fd402ed40bfb4251ec39d8be
Oybek2-web <cosmoflow49@gmail.com>
2026-07-18 16:59:22 +0500

    eleven commit

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('fanlar.urls', namespace='fanlar')),
     path('darsliklar/', include('darsliklar.urls')),
-    path('accounts/', include('accounts.urls', namespace='accounts'))
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('tolov/', include('tolov.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
